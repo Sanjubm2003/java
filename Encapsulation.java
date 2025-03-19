faf507ba0df7fa5646d0a50dba4724be1aae55ea
Sanjubm2003 <sanjuabm0@gmail.com>
2025-03-19 14:18:03 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,7 +1,7 @@
 package Oops;
 
 public class Encapsulation {
-    private static String name = "Akbar";
+    private static String name = "Sanjay_BM";
 
     public String getName(){
         return name;
